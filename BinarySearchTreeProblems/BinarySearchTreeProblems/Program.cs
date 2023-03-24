18f2a932f6b37595bcf912623a342dab68c6e417
Prachi <prachikale136@gmail.com>
2023-03-25 01:50:19 +0530

    [Prachi] Add: Binary search tree

---
+++ BinarySearchTreeProblems/BinarySearchTreeProblems/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BinarySearchTreeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(20);
+            binarySearchTree.Insert(10);
+            binarySearchTree.Insert(40);
+            binarySearchTree.Display();
+
+        }
+    }
+}
